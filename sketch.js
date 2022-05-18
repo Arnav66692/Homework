6a3e921ed43bacfb847c062f12c9681848deb2b5
Arnav66692 <105764398+Arnav66692@users.noreply.github.com>
2022-05-17 20:03:01 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function setup() {
+  createCanvas(800,800);
+  box = createSprite(100,100,100,100);
+}
+var box;
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x+5;
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x-5;  
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y = box.position.y-5;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y = box.position.y+5;
+  }
+drawSprites();
+}
+
+
+
+
